18685ce41a0694c7cd3019e94245b443a78bea11
Rajit kumar <krajit909@gmail.com>
2024-06-13 22:13:51 +0530

    bug fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,19 +2,10 @@
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.vandebharatairways.com/', // Replace with your actual domain
-    generateRobotsTxt: true, // (optional) Generate a robots.txt file
-    sitemapSize: 7000, // Limit the number of URLs per sitemap file
-    changefreq: 'daily', // Frequency of page updates
-    priority: 0.7, // Priority of URLs
-    exclude: ['/admin/*','/private/*'], // Exclude specific paths or patterns
-    transform: async (config, path) => {
-        // Optionally transform the URL data
-        return {
-            loc: path, // The URL
-            lastmod: new Date().toISOString(),
-            changefreq: config.changefreq,
-            priority: config.priority,
-        };
-    },
+    siteUrl: 'https://www.vandebharatairways.com/', // Ensure this URL is correct
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // Adjust as needed
+    changefreq: 'daily',
+    priority: 0.8,
+    exclude: ['/private/*'], // Check for unintended exclusions
 };
