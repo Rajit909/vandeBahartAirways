1d5bd3c1b2dd55d978097f6a005090ad0cd800bb
Rajit kumar <krajit909@gmail.com>
2024-07-24 10:35:19 +0530

    bug fixed

--- src/app/(router)/destinations/page.jsx
+++ src/app/(router)/destinations/page.jsx
@@ -17,76 +17,8 @@ const Destinations = () => {
             <h1 className="mb-0">Popular Destination</h1>
           </div>
           <div className="tab-className text-center">
-            <ul className="nav nav-pills d-inline-flex justify-content-center mb-5">
-              <li className="nav-item">
-                <a
-                  className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill active"
-                  data-bs-toggle="pill"
-                  href="#tab-1"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    All
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="d-flex py-2 mx-3 border border-primary bg-light rounded-pill"
-                  data-bs-toggle="pill"
-                  href="#tab-2"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    USA
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill"
-                  data-bs-toggle="pill"
-                  href="#tab-3"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    Canada
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill"
-                  data-bs-toggle="pill"
-                  href="#tab-4"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    Europe
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill"
-                  data-bs-toggle="pill"
-                  href="#tab-5"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    China
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill"
-                  data-bs-toggle="pill"
-                  href="#tab-6"
-                >
-                  <span className="text-dark" style={{ width: "150px" }}>
-                    Singapore
-                  </span>
-                </a>
-              </li>
-            </ul>
             <div className="tab-content">
-              <div id="tab-1" className="tab-pane fade show p-0 active" data-aos="fade-right">
+              <div id="tab-1" className="tab-pane fade show p-0 active">
                 <div className="row g-4">
                   <DestinationList/>
                 </div>
