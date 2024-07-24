eedfdf70f6322ed99a79ac2532965884fc819fa7
Rajit kumar <krajit909@gmail.com>
2024-07-24 12:34:34 +0530

    bug fix

--- src/app/(router)/destinations/_components/destinationItem.jsx
+++ src/app/(router)/destinations/_components/destinationItem.jsx
@@ -4,31 +4,34 @@ import React from "react";
 const DestinationItem = ({ url, photosCount, name }) => {
   return (
     <>
-      <div className="destination-img ">
-        <div className="search-icon">
-          <a href={url} data-lightbox="destination-1">
-            <Image
-              className="img-fluid rounded w-100"
-              src={url}
-              alt="destination"
-              width={400}
-              height={150}
-              sizes="100vw"
-            />
-          </a>
+      <div className="national-item">
+        <Image
+          src={url}
+          className="img-fluid w-100 rounded"
+          alt="Image"
+          width={400}
+          height={200}
+          style={{maxHeight: "250px", minHeight:"249px"}}
+        />
+        <div className="national-content">
+          <div className="national-info">
+            <h5 className="text-white text-uppercase mb-2">{name}</h5>
+            <h5 className="text-white mb-2">{photosCount} more Photos</h5>
+            <a href="#" className="btn-hover text-white">
+              View All Place <i className="fa fa-arrow-right ms-2"></i>
+            </a>
+          </div>
         </div>
-
-        <div className="destination-overlay p-4">
-          <a
-            href="#"
-            className="btn btn-primary  rounded-pill border py-2 px-3"
-          >
-            {photosCount} Photos
-          </a>
-          <h4 className=" mb-2 mt-3">{name}</h4>
-          <a href="#" className="btn-hover text-white">
-            View All Place <i className="fa fa-arrow-right ms-2"></i>
-          </a>
+        <div className="national-plus-icon">
+          <div className="gallery-plus-icon">
+            <a
+              href={url}
+              data-lightbox="gallery-2"
+              className="my-auto"
+            >
+              <i className="fas fa-plus fa-2x text-white"></i>
+            </a>
+          </div>
         </div>
       </div>
     </>
--- src/app/(router)/destinations/page.jsx
+++ src/app/(router)/destinations/page.jsx
@@ -1,32 +1,42 @@
 "use client"
 import React from "react";
 import DestinationList from "./_components/destinationList.jsx";
+import Image from "next/image.js";
 
 
 
 const Destinations = () => {
   return (
     <>
-      <div className="container-fluid destination py-5">
-        <div className="container py-5">
-          <div
-            className="mx-auto text-center mb-5"
-            style={{ maxWidth: "900px" }}
-          >
-            <h5 className="section-title px-3">Destination</h5>
-            <h1 className="mb-0">Popular Destination</h1>
-          </div>
-          <div className="tab-className text-center">
-            <div className="tab-content">
-              <div id="tab-1" className="tab-pane fade show p-0 active">
-                <div className="row g-4">
-                  <DestinationList/>
+      <div className="container-fluid ExploreTour py-5">
+            <div className="container py-5">
+                <div className="mx-auto text-center mb-5" style={{maxWidth: "900px"}}>
+                    <h5 className="section-title px-3">Explore Destinations</h5>
+                    <h1 className="mb-4">Top destinations</h1>
                 </div>
-              </div>
+                <div className="tab-className text-center">
+                    {/* <ul className="nav nav-pills d-inline-flex justify-content-center mb-5">
+                        <li className="nav-item">
+                            <a className="d-flex mx-3 py-2 border border-primary bg-light rounded-pill active" data-bs-toggle="pill" href="#NationalTab-1">
+                                <span className="text-dark" style={{width: "250px"}}>National Tour Category</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="d-flex py-2 mx-3 border border-primary bg-light rounded-pill" data-bs-toggle="pill" href="#InternationalTab-2">
+                                <span className="text-dark" style={{width: "250px"}}>International tour Category</span>
+                            </a>
+                        </li>
+                    </ul> */}
+                    <div className="tab-content" data-aos="fade-right">
+                        <div id="NationalTab-1" className="tab-pane fade show p-0 active">
+                            <div className="row g-4">
+                              <DestinationList/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </>
   );
 };
